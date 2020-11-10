68ec42596c53797f9f6cf9ae4b4949d669579089
Jack <hy5@hotmail.co.uk>
2020-11-10 18:42:41 -0500

    Pruning

--- MBTI.ai_logic_data.class.php
+++ MBTI.ai_logic_data.class.php
@@ -13,13 +13,12 @@ class MBTI_logic_data extends ai_extended_logic
         ),
         'discord_contributors' => array(
             'tirone.png' => array( 'data' => 'Admin#9999' ),
-            '69.png' => array( 'data' => '6un9#0408' ),
             'angel.png' => array( 'data' => 'Cyrus#3693' ),
             'stifler.png' => array( 'data' => 'StiflerCosmin#3659' )
         ),
         'primary_contributors' => array(
-            'jack.png' => array( 'data' => '' ),
-            'linda.png' => array( 'data' => '' )
+            'jack.png' => array( 'data' => '' )
+            // 'linda.png' => array( 'data' => '' )
         )
     );
 
